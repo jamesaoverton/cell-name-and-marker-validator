c0f9cbd01dd2b5e220e29ed2357a65b1987c893e
Michael Cuffaro <mike@gwydion>
2018-09-17 18:38:12 -0400

    fix proken unittest for report.py

--- src/report.py
+++ src/report.py
@@ -1,49 +1,64 @@
 #!/usr/bin/env python3
 
-import argparse, csv, re
+import argparse
+import csv
+import re
+
+
+def generate_report_row(row):
+  normalized = row['Gating mapped to ontologies']
+  if not normalized:
+    return None
+
+  ids = re.split(';\s+', normalized)
+  matched_gates = 0
+  total_gates = 0
+  for i in ids:
+    total_gates += 1
+    if not i.startswith('!'):
+      matched_gates += 1
+  row['MATCHED_GATES'] = matched_gates
+  row['TOTAL_GATES'] = total_gates
+  return row
+
 
 def main():
-  parser = argparse.ArgumentParser(
-      description='Parse HIPC cell')
-  parser.add_argument('normalized',
-      type=argparse.FileType('r'),
-      help='the normalized TSV file')
-  parser.add_argument('gates',
-      type=argparse.FileType('r'),
-      help='the gates TSV file')
-  parser.add_argument('output',
-      type=str,
-      help='the output TSV file')
+  parser = argparse.ArgumentParser(description='Parse HIPC cell')
+  parser.add_argument('normalized', type=argparse.FileType('r'), help='the normalized TSV file')
+  parser.add_argument('gates', type=argparse.FileType('r'), help='the gates TSV file')
+  parser.add_argument('output', type=str, help='the output TSV file')
   args = parser.parse_args()
-
-  gate_types = {}
-  rows = csv.reader(args.gates, delimiter='\t')
-  for row in rows:
-    if row[1] and row[0] and row[0] != '':
-      gate_types[row[1]] = row[0]
 
   rows = csv.DictReader(args.normalized, delimiter='\t')
   with open(args.output, 'w') as output:
     w = csv.writer(output, delimiter='\t', lineterminator='\n')
-    w.writerow(list(next(rows).keys()) + ['MATCHED_GATES', 'TOTAL_GATES'])
+    output_fieldnames = rows.fieldnames + ['MATCHED_GATES', 'TOTAL_GATES']
+    w.writerow(output_fieldnames)
     for row in rows:
-      normalized = row['Gating mapped to ontologies']
-      if normalized:
-        ids = re.split(';\s+', normalized)
-        matched_gates = 0
-        total_gates = 0
-        for i in ids:
-          total_gates += 1
-          if not i.startswith('!'):
-            matched_gates += 1
-        row['MATCHED_GATES'] = matched_gates
-        row['TOTAL_GATES'] = total_gates
-        w.writerow(row.values())
+      report_row = generate_report_row(row)
+      if report_row:
+        w.writerow([row[fn] for fn in output_fieldnames])
+
 
 if __name__ == "__main__":
   main()
 
+
 def test_report():
-  assert report({'foo': 'PR:0'}, 'foo+ : bar+ foo- bat foo') == [
-      'PR:0+', '[:]', '[bar]+', 'PR:0-', '[bat]', 'PR:0'
-  ]
+  from copy import copy
+
+  inrow = {
+    'EXPERIMENT_ACCESSION': 'EXP14857',
+    'Gating mapped to ontologies': 'PR:000001889-; PR:000001892-; !CD3+; PR:000001004+',
+    'Gating preferred labels': 'CD14-; CD33-; !CD3+; CD4+',
+    'Gating tokenized': 'CD14-; CD33-; CD3+; CD4+',
+    'NAME': 'HIPC Stanford Project',
+    'POPULATION_DEFNITION_REPORTED': 'CD14-CD33-/CD3+/CD4+',
+    'POPULATION_NAME_REPORTED': 'CD4+ T cells',
+    'STUDY_ACCESSION': 'SDY887'}
+  expected_outrow = copy(inrow)
+  expected_outrow.update({'MATCHED_GATES': 3, 'TOTAL_GATES': 4})
+  actual_outrow = generate_report_row(inrow)
+  assert len(actual_outrow) == len(expected_outrow)
+  for k, v in expected_outrow.items():
+    assert actual_outrow[k] == v
