0b06fa329c3a9b7b64283124aafb30a77a93593a
Michael Cuffaro <mike@gwydion>
2018-09-28 18:51:13 -0400

    major refactoring of server.py (work in progress)

--- src/server.py
+++ src/server.py
@@ -58,184 +58,7 @@ level_iris = {
 iri_levels = {v: k for k, v in level_iris.items()}
 
 
-def decorate_gate(kind, level):
-  gate = {
-    'kind': kind,
-    'kind_recognized': False,
-    'level': level,
-    'level_recognized': False,
-  }
-
-  if kind in iri_labels:
-    gate['kind_recognized'] = True
-    gate['kind_label'] = iri_labels[kind]
-  if kind and not kind.startswith('http'):
-    gate['kind'] = '?gate=' + kind
-
-  if level in iri_labels:
-    gate['level_recognized'] = True
-    gate['level_label'] = iri_labels[level]
-
-  return gate
-
-
-def process_gate(gate_string):
-  for suffix in suffixsyns.keys():
-    if gate_string.endswith(suffix):
-      gate_string = re.sub('\s*' + re.escape(suffix) + '$', suffixsymbs[suffixsyns[suffix]],
-                           gate_string)
-      continue
-
-  kind_name = gate_string.rstrip('+-~')
-  kind_name = re.sub('\[.*\]', '', kind_name)
-  level_name = re.search('[\-\+\~]*$', gate_string).group(0)
-  kind = None
-  if kind_name in synonym_iris:
-    kind = synonym_iris[kind_name]
-  level = None
-  if level_name == '':
-    level_name = '+'
-  if level_name in level_iris:
-    level = level_iris[level_name]
-  gate = {}
-
-  has_errors = False
-  gate = decorate_gate(kind, level)
-  if not kind:
-    has_errors = True
-  gate['gate'] = gate_string
-  gate['kind_name'] = kind_name
-  gate['level_name'] = level_names[level_name]
-
-  return gate, has_errors
-
-
-def get_cell_info(cell_gates):
-  # Initialise a dictionary which will contain information about this cell
-  cell = {'recognized': False, 'conflicts': False, 'has_cell_gates': len(cell_gates) > 0,
-          'cell_gates': cell_gates}
-  if cell_iri in iri_gates:
-    # If the cell IRI is in the IRI->Gates map, then add its IRI and flag it as recognised.
-    cell['recognized'] = True
-    cell['iri'] = cell_iri
-    if cell_iri in iri_labels:
-      # If the cell is in the IRI->Labels map, then add its label
-      cell['label'] = iri_labels[cell_iri]
-    if cell_iri in iri_parents:
-      # It it is in the IRI->Parents map, then add its parent's IRI
-      cell['parent'] = iri_parents[cell_iri]
-      if cell['parent'] in iri_labels:
-        # If its parent's IRI is in the IRI->Labels map, then add its parent's label
-        cell['parent_label'] = iri_labels[cell['parent']]
-
-  return cell
-
-
-def get_gate_info_for_cell(cell_iri):
-  cell_results = []
-
-  if cell_iri:
-    # For each gate associated with the cell IRI, create a dictionary with information about it
-    # and append it to cell_results
-    for gate in iri_gates[cell_iri]:
-      gate = decorate_gate(gate['kind'], gate['level'])
-      if gate['level'] in iri_levels:
-        gate['level_name'] = level_names[iri_levels[gate['level']]]
-      cell_results.append(gate)
-
-  return cell_results
-
-
-@app.route('/', methods=['GET'])
-def my_app():
-  if 'gate' in request.args:
-    special_gate = request.args['gate'].strip()
-    return render_template('/gate.html', special_gate=special_gate)
-
-  # cells_field holds cell population names from the cell ontology database; if not specified, it's
-  # initialised to the following default value:
-  cells_field = 'CD4-positive, alpha-beta T cell & CD19-'
-
-  # If the request specifies the 'cells' field, parse it into a cell name and (optionally) a list of
-  # gates for that cell, using '&' as our separator. Then process each gate in the list
-  if 'cells' in request.args:
-    cells_field = request.args['cells'].strip()
-
-  cell_gates = []
-  if '&' in cells_field:
-    cells_fields = cells_field.split('&', maxsplit=1)
-    cell_name = cells_fields[0].strip()
-    cell_gating = cells_fields[1].strip()
-    # Gates are assumed to be separated by semicolons
-    gate_strings = re.split(r';\s*', cell_gating)
-    for gate_string in gate_strings:
-      gate = process_gate(gate_string)
-      cell_gates.append(gate)
-  else:
-    cell_name = cells_field
-
-  # gates_field holds gate names from the protein ontology database; if not specified, it gets
-  # initialised to the following default value, otherwise get it from the request.
-  gates_field = 'CD4-; CD19+; CD20-; CD27++; CD38+-; infected[Dengue virus]; CD56[glycosylated]+'
-  if 'gates' in request.args:
-    gates_field = request.args['gates'].strip()
-
-  # Find the IRI for the cell based on cell_name, which can be a: label/synonym, ID, or IRI.
-  if cell_name in synonym_iris:
-    cell_iri = synonym_iris[cell_name]
-  elif cell_name in iri_labels:
-    cell_iri = cell_name
-  else:
-    iri = re.sub('^CL:', 'http://purl.obolibrary.org/obo/CL_', cell_name)
-    cell_iri = iri if iri in iri_labels else None
-
-  cell = get_cell_info(cell_gates)
-  # Include the information from cell_gates (the gates specified in the request) to cell_results
-  # (the list of gates extracted based on the cell's IRI)
-  cell_results = get_gate_info_for_cell(cell.get('iri')) + cell_gates
-
-  gate_results = []
-  conflicts = []
-  gate_errors = False
-  # Parse gates_field (which is taken from the request). Assume gates are separated by semicolons
-  gate_strings = re.split(r';\s*', gates_field)
-  for gate_string in gate_strings:
-    gate, has_errors = process_gate(gate_string)
-    if has_errors and not gate_errors:
-      gate_errors = True
-
-    # Check for any discrepancies between what has been given through the request and the gate info
-    # that has been extracted (cell_results) based on a lookup of the cell IRIs. Indicate any such
-    # in the info for the gate, and append the gate info to a list of gates with conflicts. Either
-    # way, append the gate into to the gate_results list.
-    for cell_result in cell_results:
-      if gate['kind'] == cell_result['kind'] and gate['level'] != cell_result['level']:
-        gate['conflict'] = True
-        cell_result['conflict'] = True
-        cell['conflicts'] = True
-        conflict = copy.deepcopy(gate)
-        conflict['cell_level'] = cell_result['level']
-        conflict['cell_level_name'] = cell_result['level_name']
-        conflicts.append(conflict)
-    gate_results.append(gate)
-
-  # Serve the web page back with the generated info
-  return render_template(
-    '/index.html',
-    cells=cells_field,
-    gates=gates_field,
-    cell=cell,
-    cell_results=cell_results,
-    gate_results=gate_results,
-    gate_errors=gate_errors,
-    conflicts=conflicts)
-
-
-if __name__ == '__main__':
-  """
-  At startup, the main function reads information from files in the build directory and uses it to
-  populate our global dictionaries.
-  """
+def populate_maps():
   def update_maps(to_iris={}, from_iris={}):
     # This inner function updates the synonyms_iris map with the contents of to_iris, and the
     # iri_labels map with the contents of from_iris.
@@ -318,7 +141,221 @@ if __name__ == '__main__':
             }
             iri_gates[iri].append(gate)
 
-  label_iris = {v: k for k, v in iri_labels.items()}
 
+def decorate_gate(kind, level):
+  # Create and return a dictionary with information on the supplied gate type and level
+  gate = {
+    'kind': kind,
+    'kind_recognized': False,
+    'level': level,
+    'level_recognized': False,
+  }
+
+  if kind in iri_labels:
+    gate['kind_recognized'] = True
+    gate['kind_label'] = iri_labels[kind]
+  if kind and not kind.startswith('http'):
+    gate['kind'] = '?gate=' + kind
+
+  if level in iri_labels:
+    gate['level_recognized'] = True
+    gate['level_label'] = iri_labels[level]
+
+  return gate
+
+
+def process_gate(gate_string):
+  # If the gate string has a suffix which is a synonym of one of the standard suffixes, then replace
+  # it with the standard suffix:
+  for suffix in suffixsyns.keys():
+    if gate_string.endswith(suffix):
+      gate_string = re.sub('\s*' + re.escape(suffix) + '$', suffixsymbs[suffixsyns[suffix]],
+                           gate_string)
+      continue
+
+  # The 'kind' is the root of the gate string without the suffix, and the 'level' is the suffix
+  kind_name = gate_string.rstrip('+-~')
+  kind_name = re.sub('\[.*\]', '', kind_name)
+  level_name = re.search('[\-\+\~]*$', gate_string).group(0)
+  kind = None
+  if kind_name in synonym_iris:
+    kind = synonym_iris[kind_name]
+  level = None
+  if level_name == '':
+    level_name = '+'
+  if level_name in level_iris:
+    level = level_iris[level_name]
+  gate = {}
+
+  has_errors = False
+  gate = decorate_gate(kind, level)
+  if not kind:
+    has_errors = True
+  gate['gate'] = gate_string
+  gate['kind_name'] = kind_name
+  gate['level_name'] = level_names[level_name]
+
+  return gate, has_errors
+
+
+def get_cell_name_and_gates(cells_field):
+  # Parse out the name and gate list from the given cells field
+  cell_gates = []
+  if '&' in cells_field:
+    cells_fields = cells_field.split('&', maxsplit=1)
+    cell_name = cells_fields[0].strip()
+    cell_gating = cells_fields[1].strip()
+    # Gates are assumed to be separated by semicolons
+    gate_strings = re.split(r';\s*', cell_gating)
+    for gate_string in gate_strings:
+      gate, has_errors = process_gate(gate_string)
+      cell_gates.append(gate)
+  else:
+    cell_name = cells_field
+
+  return cell_name, cell_gates
+
+
+def get_cell_core_info(cell_gates, cell_iri):
+  # Initialise a dictionary which will contain information about this cell
+  cell = {'recognized': False, 'conflicts': False, 'has_cell_gates': len(cell_gates) > 0,
+          'cell_gates': cell_gates}
+  if cell_iri in iri_gates:
+    # If the cell IRI is in the IRI->Gates map, then add its IRI and flag it as recognised.
+    cell['recognized'] = True
+    cell['iri'] = cell_iri
+    if cell_iri in iri_labels:
+      # If the cell is in the IRI->Labels map, then add its label
+      cell['label'] = iri_labels[cell_iri]
+    if cell_iri in iri_parents:
+      # It it is in the IRI->Parents map, then add its parent's IRI
+      cell['parent'] = iri_parents[cell_iri]
+      if cell['parent'] in iri_labels:
+        # If its parent's IRI is in the IRI->Labels map, then add its parent's label
+        cell['parent_label'] = iri_labels[cell['parent']]
+
+  return cell
+
+
+def get_gate_info_for_cell(cell_iri):
+  cell_results = []
+
+  if cell_iri:
+    # For each gate associated with the cell IRI, create a dictionary with information about it
+    # and append it to cell_results
+    for gate in iri_gates[cell_iri]:
+      gate = decorate_gate(gate['kind'], gate['level'])
+      if gate['level'] in iri_levels:
+        gate['level_name'] = level_names[iri_levels[gate['level']]]
+      cell_results.append(gate)
+
+  return cell_results
+
+
+def get_cell_iri(cell_name):
+  # Find the IRI for the cell based on cell_name, which can be a: label/synonym, ID, or IRI.
+  if cell_name in synonym_iris:
+    cell_iri = synonym_iris[cell_name]
+  elif cell_name in iri_labels:
+    cell_iri = cell_name
+  else:
+    iri = re.sub('^CL:', 'http://purl.obolibrary.org/obo/CL_', cell_name)
+    cell_iri = iri if iri in iri_labels else None
+
+  return cell_iri
+
+
+def parse_cells_field(cells_field):
+  cell = {}
+  # Extract the cell name and the gates specified in the cells field of the request string
+  cell['name'], cell['gates'] = get_cell_name_and_gates(cells_field)
+  # Get the cell and gate information for the gates specified in the cells field
+  cell['iri'] = get_cell_iri(cell['name'])
+  cell['core_info'] = get_cell_core_info(cell['gates'], cell['iri'])
+  # Include the information from cell_gates (the gates specified in the request) to cell_results
+  # (the list of gates extracted based on the cell's IRI)
+  cell['results'] = get_gate_info_for_cell(cell['iri']) + cell['gates']
+  return cell
+
+
+def parse_gates_field(gates_field, cell):
+  gating = {'results': [], 'conflicts': [], 'has_errors': False}
+  # Assume gates are separated by semicolons
+  gate_strings = re.split(r';\s*', gates_field)
+  for gate_string in gate_strings:
+    gate, has_errors = process_gate(gate_string)
+    if has_errors and not gating['has_errors']:
+      gating['has_errors'] = True
+
+    # Check for any discrepancies between what has been given through the request and the gate info
+    # that has been extracted (cell_results) based on a lookup of the cell IRIs. Indicate any such
+    # in the info for the gate, and append the gate info to a list of gates with conflicts. Either
+    # way, append the gate into to the gate_results list.
+    for cell_result in cell['results']:
+      if gate['kind'] == cell_result['kind'] and gate['level'] != cell_result['level']:
+        gate['conflict'] = True
+        cell_result['conflict'] = True
+        cell['core_info']['conflicts'] = True
+        conflict = copy.deepcopy(gate)
+        conflict['cell_level'] = cell_result['level']
+        conflict['cell_level_name'] = cell_result['level_name']
+        gating['conflicts'].append(conflict)
+    gating['results'].append(gate)
+
+  return gating
+
+
+@app.route('/', methods=['GET'])
+def my_app():
+  if 'gate' in request.args:
+    special_gate = request.args['gate'].strip()
+    return render_template('/gate.html', special_gate=special_gate)
+
+  # cells_field holds cell population names from the cell ontology database; if not specified, it's
+  # initialised to the following default value. Otherwise we get it from the request
+  cells_field = 'CD4-positive, alpha-beta T cell & CD19-'
+  if 'cells' in request.args:
+    cells_field = request.args['cells'].strip()
+
+  # Parse the cells_field
+  cell = parse_cells_field(cells_field)
+
+  # gates_field holds gate names from the protein ontology database; if not specified, it gets
+  # initialised to the following default value, otherwise get it from the request.
+  gates_field = 'CD4-; CD19+; CD20-; CD27++; CD38+-; infected[Dengue virus]; CD56[glycosylated]+'
+  if 'gates' in request.args:
+    gates_field = request.args['gates'].strip()
+
+  # Parse the gates_field
+  gating = parse_gates_field(gates_field, cell)
+
+  # Serve the web page back with the generated info
+  return render_template(
+    '/index.html',
+    cells=cells_field,
+    gates=gates_field,
+    cell=cell['core_info'],
+    cell_results=cell['results'],
+    gate_results=gating['results'],
+    gate_errors=gating['has_errors'],
+    conflicts=gating['conflicts'])
+
+
+if __name__ == '__main__':
+  """
+  At startup, the main function reads information from files in the build directory and uses it to
+  populate our global dictionaries. It then starts the Flask application.
+  """
+  populate_maps()
   app.debug = True
   app.run()
+
+
+def test_server():
+  populate_maps()
+  cells_field = 'CD4-positive, alpha-beta T cell & CD19-'
+  gates_field = 'CD4-; CD19+; CD20-; CD27++; CD38+-; infected[Dengue virus]; CD56[glycosylated]+'
+  cell = parse_cells_field(cells_field)
+  gating = parse_gates_field(gates_field, cell)
+
+  # TODO: ASSERT AGAINST THE OLD VERSION OF THE OUTPUT
